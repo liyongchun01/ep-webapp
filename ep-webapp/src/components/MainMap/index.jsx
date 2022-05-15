5c81552a4f0964bbc33bee054e71659bcb938d5f
liyongchun <lycjuly@163.com>
2022-05-15 13:03:47 +0800

    feat: 地图新增点聚合

--- ep-webapp/src/components/MainMap/index.jsx
+++ ep-webapp/src/components/MainMap/index.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react'
 import * as mapUtils from '@/components/TMapGL'
 import axios from 'axios'
-import markerLogo from './img/markerLogo.png'
 import { Spin } from 'antd'
 
 export default ({ type }) => {
@@ -38,50 +37,47 @@ export default ({ type }) => {
                 })
 
                 const ipLocation = new TMap.service.IPLocation()
-                const marker = new TMap.MultiMarker({
+                const markerCluster = new TMap.MarkerCluster({
+                    id: 'cluster',
                     map: map,
-                    styles: {
-                        //创建一个styleId为"myStyle"的样式（styles的子属性名即为styleId）
-                        "centerStyle": new TMap.MarkerStyle({
-                            "width": 32, // 点标记样式宽度（像素）
-                            "height": 32, // 点标记样式高度（像素）
-                            "src": markerLogo, //图片路径
-                            //焦点在图片中的像素位置，一般大头针类似形式的图片以针尖位置做为焦点，圆形点以圆心位置为焦点
-                            "anchor": { x: 16, y: 32 }
-                        })
-                    }
-                })
+                    enableDefaultStyle: true, // 启用默认样式
+                    minimumClusterSize: 2, // 形成聚合簇的最小个数
+                    geometries: [],
+                    zoomOnClick: false, // 点击簇时放大至簇内点分离
+                    gridSize: 60, // 聚合算法的可聚合距离
+                    averageCenter: true, // 每个聚和簇的中心是否应该是聚类中所有标记的平均值
+                    maxZoom: 10, // 采用聚合策略的最大缩放级别
+                });
 
                 // 定位
                 const locate = () => {
-                    const ipInput = document.getElementById('ipInput').value
-                    const params = ipInput ? { ip: ipInput } : {}
                     ipLocation
-                        .locate(params)
-                        .then((result2) => {
-                            // 未给定ip地址则默认使用请求端的ip
-                            let { result } = result2
-                            marker.updateGeometries([
-                                {
-                                    id: 'main',
-                                    styleId: 'centerStyle',
-                                    position: result.location, // 将所得位置绘制在地图上
-                                },
-                            ])
+                        .locate()
+                        .then(({ result }) => {
                             map.setCenter(result.location)
-                            document.getElementById(
-                                'ipLocationResult'
-                            ).innerText = `您的IP/您输入的IP所在位置: ${result.ad_info.nation}, ${result.ad_info.province}`
-                        })
-                        .catch((error) => {
-                            document.getElementById(
-                                'ipLocationResult'
-                            ).innerText = `错误：${error.status}, ${error.message}`
                         })
                 }
 
                 locate()
                 const infoWindowList = Array(10)
+
+                const infoContent = (item) => {
+                    const info = {
+                        1: {
+                            content: `  核酸检测信息 <br/> <h3>${item.hesuanPosition}</h3> ${item.hesuanName} <br/> 工作时间：${item.starttime} - ${item.endtime} <br /> 最新时间: ${item.area} 人数: ${item.renshu}`,
+                        },
+                        2: {
+                            content: `  疫苗接种信息 <br/> <h3>${item.yimiaoPosition}</h3> ${item.yimaioName} <br/>工作时间：${item.starttime} - ${item.endtime}  <br/>所属机构：${item.orgType} <br/> 第${item.batch}批次 <br /> 最新时间: ${item.area} 人数: ${item.renshu}`,
+                        },
+                        3: {
+                            content: ` 隔离地点信息 <br/>  <h3>${item.gelidianPosition}</h3> ${item.gelidianName} <br/>所属机构：${item.geliOrg} <br /> 最新时间: ${item.area} 隔离人数: ${item.grlirenshu} <br /> 联系电话${item.contact}`,
+                        },
+                        4: {
+                            content: ` 排查确诊者轨迹信息 <br/> <h3>${item.guijiPosition}</h3>  时间：${item.starttime} - ${item.endtime}`,
+                        }
+                    }
+                    return info
+                }
 
                 const getLocationList = async ({ dbWeiDu, dbJingDu, xnWeiDu, xnJingDu }) => {
                     setIsloading(true)
@@ -95,24 +91,7 @@ export default ({ type }) => {
                     })
                     setIsloading(false)
                     data[type]?.forEach((item, index) => {
-                        const infoContent = (item) => {
-                            const info = {
-                                1: {
-                                    content: `  核酸检测信息 <br/> <h3>${item.hesuanPosition}</h3> ${item.hesuanName} <br/> 工作时间：${item.starttime} - ${item.endtime} <br /> 最新时间: ${item.area} 人数: ${item.renshu}`,
-                                },
-                                2: {
-                                    content: `  疫苗接种信息 <br/> <h3>${item.yimiaoPosition}</h3> ${item.yimaioName} <br/>工作时间：${item.starttime} - ${item.endtime}  <br/>所属机构：${item.orgType} <br/> 第${item.batch}批次 <br /> 最新时间: ${item.area} 人数: ${item.renshu}`,
-                                },
-                                3: {
-                                    content: ` 隔离地点信息 <br/>  <h3>${item.gelidianPosition}</h3> ${item.gelidianName} <br/>所属机构：${item.geliOrg} <br /> 最新时间: ${item.area} 隔离人数: ${item.grlirenshu} <br /> 联系电话${item.contact}`,
-                                },
-                                4: {
-                                    content: ` 排查确诊者轨迹信息 <br/> <h3>${item.guijiPosition}</h3>  时间：${item.starttime} - ${item.endtime}`,
-                                }
-                            }
-                            return info
-                        }
-                        const geometries = marker.getGeometries()
+                        const geometries = markerCluster.getGeometries()
                         const infoWindow = new TMap.InfoWindow({
                             map: map,
                             position: formatLatLng(item.weidu, item.jindu),
@@ -125,43 +104,25 @@ export default ({ type }) => {
                             id: String(index), // 点标注数据数组
                             position: formatLatLng(item.weidu, item.jindu),
                         })
-                        marker.updateGeometries(geometries) // 绘制地点标注
-                        marker.on('click', (e) => {
+                        markerCluster.updateGeometries(geometries) // 绘制地点标注
+                        markerCluster.on('click', ({ cluster }) => {
                             infoWindow.close()
-                            let formatId = e.geometry.id
-                            if (isNaN(+e.geometry.id)) {
+                            let formatId = cluster.geometries[0].id
+                            if (isNaN(+formatId)) {
                                 formatId = formatId.split("-")[0]
                             }
-                            if (formatId == index) {
-                                console.log(infoContent(item)[type].content)
+                            if (+formatId === index) {
                                 infoWindowList[Number(formatId)]?.setContent(
                                     infoContent(item)[type].content
                                 )
+                                infoWindowList[Number(formatId)]?.open()
                             }
-                            infoWindowList[Number(formatId)]?.open()
-                        }) // 点击标注显示信息窗体
-                        if (type === 4) {
-                            marker.on('rightclick', (e) => {
-                                infoWindow.close()
-                                let formatId = e.geometry.id
-                                if (isNaN(+e.geometry.id)) {
-                                    formatId = formatId.split("-")[0]
-                                }
-                                if (formatId == index) {
-                                    // 返回行为信息方法及操作
-                                    console.log(item)
-                                    infoWindowList[Number(formatId)]?.setContent(
-                                        '123123123' // 返回行为的占位符
-                                    )
-                                    infoWindowList[Number(formatId)]?.open()
-                                }
-                            })// 右键单击显示行为信息   
-                        }
+                        })
                     })
                 }
 
                 map.on("bounds_changed", debounce(() => {
-                    const mapCenter = map.getCenter() //获取地图中心点坐标          
+                    const mapCenter = map.getCenter() //获取地图中心点坐标
                     const mapBounds = map.getBounds() //获取当前地图的视野范围
                     const rangeObj = {}
                     if (mapBounds) {
@@ -184,11 +145,6 @@ export default ({ type }) => {
         <>
             <Spin spinning={isLoading}>
                 <div id='container'></div>
-                <div id="panel" style={{ display: 'none' }}>
-                    <p><input type='text' id='ipInput' placeholder="输入IP地址(默认为请求端的IP)" size='30' /><input type='button' id='locate'
-                        value='搜索所在位置' /></p>
-                    <p id="ipLocationResult"></p>
-                </div>
             </Spin>
         </>
     )
